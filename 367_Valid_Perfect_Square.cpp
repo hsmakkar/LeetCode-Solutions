5d9d869ec6efb6da3296906544be62585a4ce16d
hsmakkar <hs.makkar18@gmail.com>
2020-05-31 10:32:31 +0530

    Create 367_Valid_Perfect_Square.cpp

---
+++ 367_Valid_Perfect_Square.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool isPerfectSquare(int num) {
+        int d=sqrt(num);
+        if(d*d==num)
+            return true;
+        else
+            return false;
+        
+    }
+};
