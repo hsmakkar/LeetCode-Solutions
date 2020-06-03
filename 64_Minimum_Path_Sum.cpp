0a073f78fa71c2f00f148ba200c498f219a1616c
hsmakkar <hs.makkar18@gmail.com>
2020-06-03 17:29:58 +0530

    Create 64_Minimum_Path_Sum.cpp

---
+++ 64_Minimum_Path_Sum.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int minPathSum(vector<vector<int>>& grid) {
+        int m=grid.size();
+        int n=grid[0].size();
+        int a[m][n];
+        memset(a,0,sizeof(a));
+        int i,j;
+        a[0][0]=grid[0][0];
+        for(i=1;i<m;i++)
+        {
+            a[i][0]=grid[i][0]+a[i-1][0];
+        }
+        for(i=1;i<n;i++)
+        {
+            a[0][i]=grid[0][i]+a[0][i-1];
+        }
+        for(i=1;i<m;i++)
+        {
+            for(j=1;j<n;j++)
+            {
+                a[i][j]=min(a[i-1][j],a[i][j-1])+grid[i][j];
+            }
+        }
+        return a[m-1][n-1];
+    }
+};
