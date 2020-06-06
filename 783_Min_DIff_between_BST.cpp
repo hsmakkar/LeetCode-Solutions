26d81231e1b07c1f0ada839c035a709d7c0a61d3
hsmakkar <hs.makkar18@gmail.com>
2020-06-06 10:01:14 +0530

    Create 783_Min_DIff_between_BST.cpp

---
+++ 783_Min_DIff_between_BST.cpp
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+void inorder(TreeNode *root,vector<int>&v)
+{
+    if(!root)
+        return;
+    inorder(root->left,v);
+    v.push_back(root->val);
+    inorder(root->right,v);
+}
+class Solution {
+public:
+    int minDiffInBST(TreeNode* root) {
+        vector<int>v;
+        inorder(root,v);
+        int i;int mn=INT_MAX;
+        for(i=1;i<v.size();i++)
+        {
+            if(v[i]-v[i-1]<mn)
+                mn=v[i]-v[i-1];
+        }
+        return mn;
+        
+    }
+};
