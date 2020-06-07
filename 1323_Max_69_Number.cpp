7f70b3f62fed321fe49b55fe56c1d617b2545023
hsmakkar <hs.makkar18@gmail.com>
2020-06-07 16:12:11 +0530

    Create 1323_Max_69_Number.cpp

---
+++ 1323_Max_69_Number.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int maximum69Number (int num) {
+        string s=to_string(num);
+        int i;
+        for(i=0;i<s.size();i++)
+        {
+            if(s[i]=='6')
+            {
+                s[i]='9';
+                break;
+            }
+        }
+        int n=stoi(s);
+        return n;
+        
+    }
+};
