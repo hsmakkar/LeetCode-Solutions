3c0fb0c50a65a1c4f5840a3bc255268ead05f040
hsmakkar <hs.makkar18@gmail.com>
2020-06-16 14:46:54 +0530

    Create 628_Maximum_Product.cpp

---
+++ 628_Maximum_Product.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int maximumProduct(vector<int>& a) {
+        sort(a.begin(),a.end());
+        int n=a.size();
+        
+        
+        return max(a[0]*a[1]*a[n-1],max(a[n-1]*a[n-2]*a[n-3],a[0]*a[n-2]*a[n-1]));
+        
+    }
+};
